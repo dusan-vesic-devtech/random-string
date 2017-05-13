45d00746a92730d152b7a09a685920e9e3c73346
vesic <dusan.vesic@gmail.com>
2017-05-13 21:13:52 +0000

    Update random string class

--- src/RandomString.php
+++ src/RandomString.php
@@ -4,19 +4,19 @@ namespace Vesic\RandomString;
 
 class RandomString {
     
+    protected static $chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    
     public static function getChars($length = 32) {
-        $chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-        
         if (!is_int($length) || $length < 0) {
             throw new Exception('Length not valid.');
         }
         
-        $chars_len = strlen($chars) - 1;
+        $chars_len = strlen(self::$chars) - 1;
         
         $output = '';
         
         foreach (range(0, $length - 1) as $value) {
-            $output .= $chars[mt_rand(0, $chars_len)];
+            $output .= self::$chars[mt_rand(0, $chars_len)];
         }
         
         return $output;
