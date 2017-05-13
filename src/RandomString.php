483b7a1bf828aaf83ec1a820f2f889dd925a8da7
vesic <dusan.vesic@gmail.com>
2017-05-13 20:18:11 +0000

    Random string generation

--- src/RandomString.php
+++ src/RandomString.php
@@ -4,8 +4,22 @@ namespace Devtech\RandomString;
 
 class RandomString {
     
-    public static function getChars() {
-        return '';
+    public static function getChars($length = 32) {
+        $chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        
+        if (!is_int($length) || $length < 0) {
+            throw new Exception('Length not valid.');
+        }
+        
+        $chars_len = strlen($chars) - 1;
+        
+        $output = '';
+        
+        foreach (range(0, $length - 1) as $value) {
+            $output .= $chars[mt_rand(0, $chars_len)];
+        }
+        
+        return $output;
     }
     
 }
